8b4cb85f13488d205795de926b8f196bef41e779
Jiwon <cutty82432@gmial.com>
2023-06-21 19:35:32 +0900

    New PJT <books>

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Date information', {'fields':['pub_date']})
+    ]    
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date'] # 생성날짜로 필터
+    search_fields = ['question_text'] # 검색박스
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
